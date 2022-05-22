9ce87b39cd61f58c2c394b33c83f5f0d34f14620
Valerii_Kuzivanov <valerii.kuzivanov@ridewithvia.com>
2022-05-22 22:47:23 +0300

    addded fronend slider

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,11 +1,11 @@
-const product = require('../models/product')
+const productController = require('../models/product')
 
 exports.getList = async (req, res) => {
-    const productList = await product.getList({ limit: 10 })
+    const productList = await productController.getList({ limit: 10 })
     res.send(productList)
 }
 
 exports.addProduct = async (req, res) => {
-    const product = await product.addProduct(req.body)
-    res.send(product)
+    const newProduct = await productController.addProduct(req.body)
+    res.send(newProduct)
 }
