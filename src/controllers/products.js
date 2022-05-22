b20c37a621201645fdeb04bf5d77454ccd7ca063
Valerii_Kuzivanov <valerii.kuzivanov@ridewithvia.com>
2022-05-22 22:48:08 +0300

    model refactoring

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,11 +1,11 @@
-const productController = require('../models/product')
+const productModel = require('../models/product')
 
 exports.getList = async (req, res) => {
-    const productList = await productController.getList({ limit: 10 })
+    const productList = await productModel.getList({ limit: 10 })
     res.send(productList)
 }
 
 exports.addProduct = async (req, res) => {
-    const newProduct = await productController.addProduct(req.body)
+    const newProduct = await productModel.addProduct(req.body)
     res.send(newProduct)
 }
