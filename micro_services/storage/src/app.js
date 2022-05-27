6db39fe26cdfdb835144ccfb4ada06a3c666da97
Valerii_Kuzivanov <valerii.kuzivanov@ridewithvia.com>
2022-05-27 22:36:30 +0300

    added ajv validator to koa app

--- micro_services/storage/src/app.js
+++ micro_services/storage/src/app.js
@@ -1,10 +1,12 @@
 const Koa = require('koa')
-const router = require('./router')
 const app = new Koa()
 const bodyParser = require('koa-bodyparser')
+const router = require('./router')
+const errorHandler = require('./middleware/global-error-handler')
 
 const port = parseInt(process.env.PORT) || 4000
 
+app.use(errorHandler)
 app.use(bodyParser())
 app.use(router.routes())
 
